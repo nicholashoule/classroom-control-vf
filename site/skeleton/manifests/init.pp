f5d0601052d62d15235a82c875442e93dfd1b158
Nicholas Houle <nmhoule@mmm.com>
2016-07-20 12:29:32 -0600

    Update skeleton class:   - Add init.pp   - Ensure files are managed   - Add .bashrc file to servers

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,18 @@
+# class skeleton.pp
+# 
+# @Authors Nicholas Houle
+# 
+# Copyright 2016
+class skeleton (){
+
+  file { 'createDir skel':
+    ensure => directory,
+    path   => '/etc/skel'
+  }
+
+  file { 'create skel':
+    ensure => file,
+    path   => '/etc/skel/.bashrc',
+    source => 'puppet:///modules/skeleton/.bashrc'
+  }
+}
