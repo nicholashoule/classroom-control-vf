00f3fc7e6b17ed6ef0f27082a92339c63a8d2d46
Nicholas Houle <nmhoule@mmm.com>
2016-07-21 10:53:37 -0600

    Configure nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -7,7 +7,12 @@ class nginx (){
 
   package { 'nginx':
     ensure => latest,
-    before => File['create nginx config']
+  }
+
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
   }
 
   file { 'createDir nginx':
@@ -18,9 +23,23 @@ class nginx (){
   file { 'create nginx config':
     ensure  => file,
     path    => '/etc/nginx/nginx.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => [ Package['nginx'], File['createDir nginx'] ],
+    notify  => Service['nginx']
+  }
+
+  file { 'create nginx conf.d config':
+    ensure  => file,
+    path    => '/etc/nginx/conf.d/default.conf',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => [ Package['nginx'], File['createDir nginx'] ],
+    notify  => Service['nginx']
+  }
+
+  file { 'create nginx html index':
+    ensure  => file,
+    path    => '/var/www/html/index.html',
+    source  => 'puppet:///modules/nginx/index.html',
     require => [ Package['nginx'], File['createDir nginx'] ],
     notify  => Service['nginx']
   }
