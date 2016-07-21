f866a4599011128f238daa48c82f67c076ccfdde
Nicholas Houle <email@email.com>
2016-07-21 16:40:18 -0600

    Start managing users.

--- site/users/manifests/managed_users.pp
+++ site/users/manifests/managed_users.pp
@@ -3,7 +3,10 @@ define users::managed_users (
     $group       = $title,
     $user        = $title,
     $shell       = '/bin/bash',
+    $enable_ssh  = true,
 ) {
+  
+  $_memory = '2048'
 
   $users.each |$item| {
     notify { $item:
@@ -29,11 +32,23 @@ define users::managed_users (
     mode   => '0700',
   }
 
-  file { "/home/${user}/.ssh":
-    ensure => directory,
-    owner  => $title,
-    group  => $group,
-    mode   => '0700',
+  if $enable_ssh == true {
+
+    file { "/home/${user}/.ssh":
+      ensure => directory,
+      owner  => $title,
+      group  => $group,
+      mode   => '0700',
+      notify => Exec["SSH key: ${user}"],
+    }
+
+    exec { "SSH key: ${user}":
+      command => "ssh-keygen -t rsa -M '${_memory}' -f /home/${user}/.ssh/id_rsa",
+      path    => [],
+      creates => "/home/${user}/.ssh/id_rsa",
+      require => File["/home/${user}/.ssh"],
+    }
   }
+
 }
 
